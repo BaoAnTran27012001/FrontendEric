6ad9e38c6aa8c744930b1db85fd613a88b509ea1
Tran Bao An <worldcoder1234@gmail.com>
2024-01-27 10:04:05 +0700

    Completed UI Design for Manage All Users

--- src/containers/System/UserManage.js
+++ src/containers/System/UserManage.js
@@ -1,33 +1,73 @@
 import React, { Component } from 'react';
 import { FormattedMessage } from 'react-intl';
 import { connect } from 'react-redux';
+import './UserManage.scss';
+import { getAllUsers } from '../../services/userService';
 class UserManage extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      arrUsers: [],
+    };
+  }
 
-    state = {
-
+  async componentDidMount() {
+    const response = await getAllUsers('ALL');
+    console.log(response);
+    if (response && response.errCode === 0) {
+      this.setState({
+        arrUsers: response.users,
+      });
     }
+  }
+  render() {
+    console.log('check render:', this.state);
+    const allUsers = this.state.arrUsers;
+    return (
+      <div className='users-container'>
+        <div className='title'>Manage users with React</div>
+        <div className='users-table mt-3 mx-1'>
+          <table id='customers'>
+            <tr>
+              <th>Email</th>
+              <th>First Name</th>
+              <th>Last Name</th>
+              <th>Address</th>
+              <th>Actions</th>
+            </tr>
 
-    componentDidMount() {
-
-    }
-
-
-    render() {
-        return (
-            <div className="text-center">Manage users</div>
-        );
-    }
-
+            {allUsers &&
+              allUsers.map((user, index) => {
+                return (
+                  <tr>
+                    <td>{user.email}</td>
+                    <td>{user.firstName}</td>
+                    <td>{user.lastName}</td>
+                    <td>{user.address}</td>
+                    <td className='d-flex'>
+                      <button className='btn-edit'>
+                        <i className='fas fa-pencil-alt'></i>
+                      </button>
+                      <button className='btn-delete'>
+                        <i className='fas fa-trash'></i>
+                      </button>
+                    </td>
+                  </tr>
+                );
+              })}
+          </table>
+        </div>
+      </div>
+    );
+  }
 }
 
-const mapStateToProps = state => {
-    return {
-    };
+const mapStateToProps = (state) => {
+  return {};
 };
 
-const mapDispatchToProps = dispatch => {
-    return {
-    };
+const mapDispatchToProps = (dispatch) => {
+  return {};
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(UserManage);
--- src/services/userService.js
+++ src/services/userService.js
@@ -1,5 +1,8 @@
 import axios from '../axios';
 const handleLoginAPI = (email, password) => {
-  return axios.post('api/login',{email,password});
+  return axios.post('api/login', { email, password });
 };
-export {handleLoginAPI}
+const getAllUsers = (inputId) => {
+  return axios.get(`/api/get-all-users?id=${inputId}`);
+};
+export { handleLoginAPI, getAllUsers };
