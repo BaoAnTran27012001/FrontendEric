9660cfc70d8bfd0c912ef25fc4a563cbacca1bac
Tran Bao An <worldcoder1234@gmail.com>
2024-01-28 16:37:31 +0700

    Completed User Modal

--- src/containers/System/UserManage.js
+++ src/containers/System/UserManage.js
@@ -3,29 +3,50 @@ import { FormattedMessage } from 'react-intl';
 import { connect } from 'react-redux';
 import './UserManage.scss';
 import { getAllUsers } from '../../services/userService';
+import ModalUser from './ModalUser';
 class UserManage extends Component {
   constructor(props) {
     super(props);
     this.state = {
       arrUsers: [],
+      isModalOpen:false
     };
   }
 
   async componentDidMount() {
     const response = await getAllUsers('ALL');
-    console.log(response);
     if (response && response.errCode === 0) {
       this.setState({
         arrUsers: response.users,
       });
     }
   }
+  handleAddNewUser = () => {
+    this.setState({
+      isModalOpen:true
+    })
+  };
+  toggleUserModal = ()=>{
+    this.setState({
+      isModalOpen:!this.state.isModalOpen
+    })
+  }
   render() {
     console.log('check render:', this.state);
     const allUsers = this.state.arrUsers;
     return (
       <div className='users-container'>
+      <ModalUser open = {this.state.isModalOpen} onHandleToggle = {this.toggleUserModal}/>
         <div className='title'>Manage users with React</div>
+        <div className='mx-1'>
+          <button
+            className='btn btn-primary px-3'
+            onClick={() => this.handleAddNewUser()}
+          >
+            <i className='fas fa-plus'></i>
+            Add new users
+          </button>
+        </div>
         <div className='users-table mt-3 mx-1'>
           <table id='customers'>
             <tr>
