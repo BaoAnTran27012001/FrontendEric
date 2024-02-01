c198b92a03d554e552cbff0cca02e676e20c3a2b
Tran Bao An <worldcoder1234@gmail.com>
2024-02-01 20:09:26 +0700

    Building Carousel

--- src/containers/HomePage/HomeHeader.js
+++ src/containers/HomePage/HomeHeader.js
@@ -136,7 +136,7 @@ class HomeHeader extends Component {
               </div>
               <div className='option-child'>
                 <div className='icon-child'>
-                  <i class='far fa-lightbulb'></i>
+                  <i className='far fa-lightbulb'></i>
                 </div>
                 <div className='text-child'>
                   <FormattedMessage id={'banner.child4'} />
