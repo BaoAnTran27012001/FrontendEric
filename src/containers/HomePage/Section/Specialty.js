8e73ac9da60f9e11d7c95a17a6dc3b3e6f4c4b77
Tran Bao An <worldcoder1234@gmail.com>
2024-02-02 09:18:41 +0700

    Customized Carousel

--- src/containers/HomePage/Section/Specialty.js
+++ src/containers/HomePage/Section/Specialty.js
@@ -7,38 +7,54 @@ import './Specialty.scss';
 import Slider from 'react-slick';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
+import specialty from '../../../assets/specialty/co-xuong-khop.jpg';
 class Specialty extends Component {
   render() {
     let settings = {
       dots: true,
       infinite: true,
       speed: 500,
-      slidesToShow: 1,
+      slidesToShow: 4,
       slidesToScroll: 1,
+      // nextArrow: <SampleNextArrow />,
+      // prevArrow: <SamplePrevArrow />
     };
     return (
       <div className='section-specialty'>
         <div className='specialty-content'>
+          <div className='specialty-header'>
+            <span className='title-section'>Chuyên khoa phổ biến</span>
+            <button className='btn-section'>Xem thêm</button>
+          </div>
+          <div className='specialty-body'>
           <Slider {...settings}>
             <div className='img-custom'>
-              <h3>1</h3>
+              <img src={specialty} alt='specialty' />
+              <div>Cơ xương khớp 1</div>
             </div>
             <div className='img-custom'>
-              <h3>2</h3>
+              <img src={specialty} alt='specialty' />
+              <div>Cơ xương khớp 1</div>
             </div>
             <div className='img-custom'>
-              <h3>3</h3>
+              <img src={specialty} alt='specialty' />
+              <div>Cơ xương khớp 1</div>
             </div>
             <div className='img-custom'>
-              <h3>4</h3>
+              <img src={specialty} alt='specialty' />
+              <div>Cơ xương khớp 1</div>
             </div>
             <div className='img-custom'>
-              <h3>5</h3>
+              <img src={specialty} alt='specialty' />
+              <div>Cơ xương khớp 1</div>
             </div>
             <div className='img-custom'>
-              <h3>6</h3>
+              <img src={specialty} alt='specialty' />
+              <div>Cơ xương khớp 1</div>
             </div>
           </Slider>
+          </div>
+          
         </div>
       </div>
     );
