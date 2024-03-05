c278f79c1839f5d7473db248958d219db5dd97b1
Tran Bao An <worldcoder1234@gmail.com>
2024-03-05 10:07:25 +0700

    Implemented MarkDown Component

--- src/containers/System/Admin/TableManageUser.js
+++ src/containers/System/Admin/TableManageUser.js
@@ -3,6 +3,22 @@ import { FormattedMessage } from 'react-intl';
 import { connect } from 'react-redux';
 import * as actions from '../../../store/actions';
 import './TableManageUser.scss';
+import * as ReactDOM from 'react-dom';
+import MarkdownIt from 'markdown-it';
+import MdEditor from 'react-markdown-editor-lite';
+// import style manually
+import 'react-markdown-editor-lite/lib/index.css';
+
+// Register plugins if required
+// MdEditor.use(YOUR_PLUGINS_HERE);
+
+// Initialize a markdown parser
+const mdParser = new MarkdownIt(/* Markdown-it options */);
+
+// Finish!
+function handleEditorChange({ html, text }) {
+  console.log('handleEditorChange', html, text);
+}
 class TableManageUser extends Component {
   constructor(props) {
     super(props);
@@ -31,44 +47,51 @@ class TableManageUser extends Component {
     console.log('hoidanit check state: ', this.state.userRedux);
     let arrUsers = this.state.userRedux;
     return (
-      <table id='TableManageUser'>
-        <tr>
-          <th>Email</th>
-          <th>First Name</th>
-          <th>Last Name</th>
-          <th>Address</th>
-          <th>Actions</th>
-        </tr>
+      <React.Fragment>
+        <table id='TableManageUser'>
+          <tr>
+            <th>Email</th>
+            <th>First Name</th>
+            <th>Last Name</th>
+            <th>Address</th>
+            <th>Actions</th>
+          </tr>
 
-        <tbody>
-          {arrUsers &&
-            arrUsers.length > 0 &&
-            arrUsers.map((item, index) => {
-              return (
-                <tr key={index}>
-                  <td>{item.email}</td>
-                  <td>{item.firstName}</td>
-                  <td>{item.lastName}</td>
-                  <td>{item.address}</td>
-                  <td>
-                    <button
-                      className='btn-edit'
-                      onClick={() => this.handleEditUser(item)}
-                    >
-                      <i className='fas fa-pencil-alt'></i>
-                    </button>
-                    <button
-                      className='btn-delete'
-                      onClick={() => this.handleDeleteUser(item)}
-                    >
-                      <i className='fas fa-trash'></i>
-                    </button>
-                  </td>
-                </tr>
-              );
-            })}
-        </tbody>
-      </table>
+          <tbody>
+            {arrUsers &&
+              arrUsers.length > 0 &&
+              arrUsers.map((item, index) => {
+                return (
+                  <tr key={index}>
+                    <td>{item.email}</td>
+                    <td>{item.firstName}</td>
+                    <td>{item.lastName}</td>
+                    <td>{item.address}</td>
+                    <td>
+                      <button
+                        className='btn-edit'
+                        onClick={() => this.handleEditUser(item)}
+                      >
+                        <i className='fas fa-pencil-alt'></i>
+                      </button>
+                      <button
+                        className='btn-delete'
+                        onClick={() => this.handleDeleteUser(item)}
+                      >
+                        <i className='fas fa-trash'></i>
+                      </button>
+                    </td>
+                  </tr>
+                );
+              })}
+          </tbody>
+        </table>
+        <MdEditor
+          style={{ height: '500px' }}
+          renderHTML={(text) => mdParser.render(text)}
+          onChange={handleEditorChange}
+        />
+      </React.Fragment>
     );
   }
 }
